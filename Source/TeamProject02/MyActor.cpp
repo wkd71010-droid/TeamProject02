66157f78408cc3380bcdd272ea67d970933a2d70
IOSERS0564\ioser <iosers0564@naver.com>
2026-03-24 10:47:39 +0900

    [Other] 새로운 액터 클래스 추가

---
+++ Source/TeamProject02/MyActor.cpp
@@ -0,0 +1,23 @@
+// MyActor.cpp
+
+
+#include "MyActor.h"
+
+AMyActor::AMyActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
